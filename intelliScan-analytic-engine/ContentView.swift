bd1e7e93191f76ae0efaac3f60f4821e635cb77d
Simon Rieger <simono41@brothertec.eu>
2023-12-15 16:30:38 +0100

    Initial Commit

---
+++ intelliScan-analytic-engine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  intelliScan-analytic-engine
+//
+//  Created by Simon Rieger on 15.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ intelliScan-analytic-engine/intelliScan_analytic_engineApp.swift
@@ -0,0 +1,17 @@
+//
+//  intelliScan_analytic_engineApp.swift
+//  intelliScan-analytic-engine
+//
+//  Created by Simon Rieger on 15.12.23.
+//
+
+import SwiftUI
+
+@main
+struct intelliScan_analytic_engineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
